b54e5a5cb3de61befaa91f5dfc89c0408fc227b1
Hu Gang <linuxbest@gmail.com>
2014-04-12 16:59:27 -0700

    adding chipscope

---
+++ pcores/axi_10gmacphy_v1_00_a/hdl/verilog/icon3.v
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014 Xilinx, Inc.
+// All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////
+//   ____  ____
+//  /   /\/   /
+// /___/  \  /    Vendor     : Xilinx
+// \   \   \/     Version    : 14.6
+//  \   \         Application: Xilinx CORE Generator
+//  /   /         Filename   : icon3.v
+// /___/   /\     Timestamp  : Sun Apr 06 21:19:32 PDT 2014
+// \   \  /  \
+//  \___\/\___\
+//
+// Design Name: Verilog Synthesis Wrapper
+///////////////////////////////////////////////////////////////////////////////
+// This wrapper is used to integrate with Project Navigator and PlanAhead
+
+`timescale 1ns/1ps
+
+module icon3(
+    CONTROL0,
+    CONTROL1,
+    CONTROL2) /* synthesis syn_black_box syn_noprune=1 */;
+
+
+inout [35 : 0] CONTROL0;
+inout [35 : 0] CONTROL1;
+inout [35 : 0] CONTROL2;
+
+endmodule
