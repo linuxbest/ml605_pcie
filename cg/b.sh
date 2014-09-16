b6a8f4f99d6cc4fb88e72fb24cd3c10b61fce650
Steve Hu <linuxbest@gmail.com>
2014-09-16 15:48:18 -0700

    auto copy module symvers

--- cg/b.sh
+++ cg/b.sh
@@ -2,10 +2,14 @@
 
 KDIR=$PWD/../linux-image/linux-3.8.0/
 
+(cd ../linux-drv && make KERNELDIR=../linux-image/linux-3.8.0/)
+
+cp ../linux-drv/Module.symvers scst/src/Module.symvers
+
 make -C iscsi-scst/usr CC="gcc -m32"
 make -C iscsi-scst/ KDIR=$KDIR
 make -C scst KDIR=$KDIR
 
-mkdir out/
+mkdir -p out/
 cp `find iscsi* scst* -name \*.ko ` out/
 cp iscsi-scst/usr/iscsi-scstd  iscsi-scst/usr/iscsi-scst-adm out/
