99e71fb9d38701d63e12c1f55f73efb52c85bb85
Steve Hu <linuxbest@gmail.com>
2014-11-19 23:09:26 -0800

    update build fifo script

--- pcores/axi_10g_eth_v1_00_a/xco/b.sh
+++ pcores/axi_10g_eth_v1_00_a/xco/b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 coregen -p coregen.cgp -b afifo73_2048_cg.xco
-coregen -p coregen.cgp -b afifo73_512_cg.xco
-coregen -p coregen.cgp -b fifo73_512.xco
-coregen -p coregen.cgp -b fifo37_512_cg.xco
+#coregen -p coregen.cgp -b afifo73_512_cg.xco
+#coregen -p coregen.cgp -b fifo73_512.xco
+#coregen -p coregen.cgp -b fifo37_512_cg.xco
